101d3f2b44fdf29d6ec293b292d6461ebc685f54
Stuart Campbell <stuart@harto.org>
2013-01-03 22:11:29 +1100

    Include lein-cljsbuild plugin dep

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject rhino-bridge "0.0.1-SNAPSHOT"
   :description "A way to export Java objects into a Rhino-based ClojureScript environment"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1450"]])
+                 [org.clojure/clojurescript "0.0-1450"]]
+  :plugins [[lein-cljsbuild "0.2.2"]]
+  :cljsbuild {:builds [{:source-path "src"}]})
